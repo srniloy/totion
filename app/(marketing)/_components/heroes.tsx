28e485ac36b40b920cca34dfccd7b4ebd65d11f2
srniloy <srniloy5009@gmail.com>
2024-02-28 16:27:54 +0600

    landing page done

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -2,8 +2,40 @@ import Image from "next/image";
 
 export const Heroes = ()=> {
     return(
-        <div>
-            Heroes
+        <div className="flex flex-col items-center justify-center
+        max-w-5xl">
+            <div className="flex items-center">
+                <div className="relative w-[300px] h-[300px] sm:w-[350px]
+                sm:h-[350px] md:w-[380px] md:h-[380px]">
+                    <Image
+                        src="/documents.png"
+                        fill
+                        className="object-contain dark:hidden"
+                        alt="document"
+                    />
+                    <Image
+                        src="/documents-dark.png"
+                        fill
+                        className="object-contain hidden dark:block"
+                        alt="document"
+                    />
+                </div>
+                <div className="relative w-[380px] h-[380px] hidden
+                md:block">
+                    <Image
+                        src='/reading.png'
+                        fill
+                        className="object-contain dark:hidden"
+                        alt="Reading"
+                    />
+                    <Image
+                        src='/reading-dark.png'
+                        fill
+                        className="object-contain hidden dark:block"
+                        alt="Reading"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
